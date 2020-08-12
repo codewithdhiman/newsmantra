8e6fd31750a5852aed5991d49971ad7d80213e85
Dhiman Chakraborty <writetodhiman7@gmail.com>
2020-08-12 23:22:39 +0530

    heroku proxy

--- src/store/action.js
+++ src/store/action.js
@@ -1,4 +1,5 @@
 const API_KEY = 'c683f72d1cc44c728add131be39c6ddd';
+const proxyurl = 'https://cors-anywhere.herokuapp.com/';
 const URL = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-09&sortBy=publishedAt&apiKey=${API_KEY}`;
 
 const fetchedItems = payload => {
@@ -13,9 +14,7 @@ const loadingItems = () => {
 const fetchPosts = () => {
     return (dispatch) => {
         dispatch(loadingItems())
-        fetch(URL, {
-            mode: 'no-cors'
-        })
+        fetch(proxyurl + URL)
             .then(res => res.json())
             .then(posts => {
                 dispatch(fetchedItems(posts.articles));
